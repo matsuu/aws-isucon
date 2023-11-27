87ea5190473644ff89338d35e9aab2481e3d0bf9
MATSUU Takuto <matsuu@gmail.com>
2023-11-28 08:17:04 +0900

    add isucon13

---
+++ isucon13/README.md
@@ -0,0 +1,66 @@
+# isucon13
+
+## Overview
+
+ISUCON13をAWS環境で構築するためのPacker Templateです。
+
+
+サーバ内の構築にはansibleを使っています。ansibleのcookbooksのみ必要な場合は[isucon/isucon13](https://github.com/isucon/isucon13)をどうぞ。
+
+## Build
+
+AMIから起動する場合はBuild不要です。
+
+```sh
+${EDITOR} variables.pkr.hcl
+packer build .
+```
+
+## Boot
+
+ご参考：[カスタム Amazon マシンイメージ (AMI) から EC2 インスタンスを起動する方法を教えてください](https://aws.amazon.com/jp/premiumsupport/knowledge-center/launch-instance-custom-ami/)
+
+## Login
+
+ubuntuユーザでsshログインが可能です。
+
+ご参考：[SSH を使用した Linux インスタンスへの接続](https://docs.aws.amazon.com/ja_jp/AWSEC2/latest/UserGuide/AccessingInstancesLinux.html)
+
+## Bench
+
+```sh
+sudo -i -u isucon
+./bench run
+```
+
+初回セットアップに時間がかかる場合があります。ベンチマークに失敗する場合はしばらく待ってからやり直してください。
+
+## 本来の設定と異なるところ
+
+* 本番ではドメインとして `*.u.isucon.dev` が使われていましたが、[devトップレベルドメインはHSTS preload-listに含まれており](https://ja.wikipedia.org/wiki/.dev)、正規のSSL証明書がないとアクセスできないため `*.u.isucon.local` に書き換えています
+* SSL証明書は自己署名のものを用意しています
+* コンテスト実施時のインスタンスタイプはc5.large(2vCPU, 4GBメモリー)が3台構成です
+
+## Manual
+
+* [ISUCON13 レギュレーション](https://isucon.net/archives/57768216.html)
+* [ISUCON13 当日マニュアル](https://github.com/isucon/isucon13/blob/c52b359fc6e733e1193ac8e9835bea23856566e7/docs/cautionary_note.md)
+* [ISUCON13 アプリケーションマニュアル](https://github.com/isucon/isucon13/blob/c52b359fc6e733e1193ac8e9835bea23856566e7/docs/isupipe.md)
+* [ISUCON13 リポジトリ](https://github.com/isucon/isucon13)
+
+### ブラウザで動作確認ができない
+
+手元のPCのhostsファイルに以下を追記してください。
+
+```
+${サーバのIPアドレス} pipe.u.isucon.local
+```
+
+追記したらブラウザから `https://pipe.u.isucon.local/` にアクセスしてみてください。
+アクセスすると証明書エラーが発生する可能性があります。強制的に受け入れるか、証明書は `/etc/nginx/tls/` 配下にあるので手元の証明書ストアに登録することで回避できるはずです。
+
+## Related
+
+* [ISUCON](https://isucon.net/)
+* [isucon/isucon13](https://github.com/isucon/isucon13)
+* [matsuu/aws-isucon](https://github.com/matsuu/aws-isucon)
---
+++ isucon13/provision.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -ex
+
+GITDIR="/tmp/isucon13"
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends ansible curl git gnupg make openssh-server openssl sudo
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends nodejs
+
+sudo snap install go --channel=1.21/stable --classic
+
+sudo rm -rf ${GITDIR}
+git clone --depth=1 https://github.com/isucon/isucon13.git ${GITDIR}
+
+# devドメインはHSTSが強制有効でブラウザでの動作確認が難しいためドメインを書き換える
+find ${GITDIR} -type f -exec sed -i -e "s/u\.isucon\.dev/u.isucon.local/g" {} +
+openssl req -subj '/CN=*.t.isucon.local' -nodes -newkey rsa:2048 -keyout ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.key -out ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.csr
+echo "subjectAltName=DNS.1:*.u.isucon.local, DNS.2:*.u.isucon.dev" > ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/extfile.txt
+openssl x509 -in ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.csr -req -signkey ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.key -sha256 -days 3650 -out ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.crt -extfile ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/extfile.txt
+cp -p ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.crt ${GITDIR}/provisioning/ansible/roles/nginx/files/etc/nginx/tls/_.u.isucon.local.issuer.crt
+mv ${GITDIR}/webapp/pdns/u.isucon.dev.zone ${GITDIR}/webapp/pdns/u.isucon.local.zone
+# 自己署名証明書を利用するため
+sed -i -e '/InsecureSkipVerify/s/false/true/' ${GITDIR}/bench/cmd/bench/benchmarker.go ${GITDIR}/bench/cmd/bench/bench.go
+
+sed -i -e "s/{{ ansible_default_ipv4.address }}/127.0.0.1/" ${GITDIR}/provisioning/ansible/roles/isucon-user/templates/env.sh
+
+sed -i -e "s/_linux_amd64//" ${GITDIR}/provisioning/ansible/roles/bench/tasks/main.yaml
+(
+
+  cd ${GITDIR}/bench
+  go build -o ../provisioning/ansible/roles/bench/files/bench -buildvcs=false ./cmd/bench
+)
+(
+  cd ${GITDIR}/frontend
+  sudo npm install -g corepack
+  make
+  sudo npm uninstall corepack
+  cp -r ./dist/ ../webapp/public/
+)
+(
+  cd ${GITDIR}/envcheck
+  CGO_ENABLED=0 go build -o ../provisioning/ansible/roles/envcheck/files/envcheck -buildvcs=false -ldflags "-s -w"
+)
+(
+  cd ${GITDIR}
+  tar zcf provisioning/ansible/roles/webapp/files/webapp.tar.gz webapp
+)
+(
+  cd ${GITDIR}/provisioning/ansible
+  ansible-playbook -i inventory/localhost application.yml
+  ansible-playbook -i inventory/localhost benchmark.yml
+)
+sudo rm -rf ${GITDIR}
+sudo apt-get purge -y ansible nodejs
+sudo apt-get autoremove -y
+
+sudo rm -f /etc/apt/sources.list.d/nodesource.list
+sudo rm -f /etc/apt/keyrings/nodesource.gpg
+
+sudo snap remove go
